a851433d22d76f2517463880739dace505178325
stepantzov <stepantzov@gmail.com>
2018-11-15 13:25:58 +0100

    Added initial pages and page actions;

--- src/main/java/pageActions/MainPageActions.java
+++ src/main/java/pageActions/MainPageActions.java
@@ -1,6 +1,24 @@
 package pageActions;
 
+import org.openqa.selenium.WebDriver;
 import pages.MainPage;
 
 public class MainPageActions extends MainPage {
+    protected static WebDriver driver;
+
+    public MainPageActions(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public static void clickAddNewComputer(){
+        elementClick(addNewComputerBtn());
+    }
+
+    public void setSearchFieldVal(String fieldValue) {
+        sendKeys(searchFld(), fieldValue);
+    }
+
+    public void clickFilterByName(){
+        elementClick(filterByNameBtn());
+    }
 }
--- src/main/java/pages/MainPage.java
+++ src/main/java/pages/MainPage.java
@@ -1,4 +1,18 @@
 package pages;
 
-public class MainPage {
+import coreFunctions.ActionsSetup;
+import org.openqa.selenium.WebElement;
+
+public class MainPage extends ActionsSetup {
+    public static WebElement addNewComputerBtn() {
+        return getElement("//a[@class='btn success']", DELAY, XPATH);
+    }
+
+    public static WebElement filterByNameBtn() {
+        return getElement("//input[@id='searchsubmit']", DELAY, XPATH);
+    }
+
+    public static WebElement searchFld() {
+        return getElement("//input[@id='searchbox']", DELAY, XPATH);
+    }
 }
